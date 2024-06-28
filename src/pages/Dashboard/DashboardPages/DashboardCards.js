baac8ae91b240c6f03c7f075b767935c2c29fe27
Anshika Jain <anshikajain2@kpmg.com>
2024-06-28 16:14:04 +0530

    udated home page images

--- src/pages/Dashboard/DashboardPages/DashboardCards.js
+++ src/pages/Dashboard/DashboardPages/DashboardCards.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles({
     zIndex: 2,
   },
   content: {
-    paddingTop: '50px',
+    paddingTop: '30px',
     height: '300px',
     overflowY: 'scroll',
   },
@@ -67,7 +67,7 @@ const studentData = [
 ];
 
 const colors = [
-  'rgba(255,99,132,0.6)','rgba(255,159,64,0.6)','rgba(255,205,86,0.6)','rgba(75,192,192,0.6)','rgba(54,162,235,0.6)','rgba(153,102,255,0.6)','rgba(201,203,207,0.6)',
+  'rgba(255,99,132,1)','rgba(255,159,64,1)','rgba(255,205,86,1)','rgba(75,192,192,1)','rgba(54,162,235,1)','rgba(153,102,255,1)','rgba(201,203,207,1)',
 ];
 
 const coloredStudentData = studentData.map((item, index) => ({
