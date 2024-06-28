fc37f3cc29da7cc21f0224fef81855a39a140a64
Anshika Jain <ansuu100@gmail.com>
2024-06-28 21:20:18 +0530

    updated home page

--- src/pages/LandingComponent/HomePage.js
+++ src/pages/LandingComponent/HomePage.js
@@ -1,45 +1,62 @@
 import React from 'react';
-import { useNavigate} from "react-router-dom";
-import '../../App.css'
-import { Card, CardContent,Button,Typography,Grid, CardMedia,Box } from '@mui/material';
-import NetworksImage from '../../images/Networks1.png'
-import Image1 from '../../images/Image1.png'
-import Image2 from '../../images/Image2.png'
-import Image3 from '../../images/Image3.png'
-import Image4 from '../../images/Image4.png'
-import Image5 from '../../images/LadyImage.png'
-import AuroLogo from '../../images/AuroLogo.png'
+import { useNavigate } from "react-router-dom";
+import { makeStyles } from '@mui/styles';
+import { Card, CardContent, Button, Typography, Grid, Box } from '@mui/material';
+
+import Image1 from '../../images/HomePageImage1.png';
+import Image2 from '../../images/HomePageImage2.png';
+import Image3 from '../../images/HomePageImage3.png';
+import Image4 from '../../images/HomePageImage4.png';
+
+import AuroLogo from '../../images/AuroLogo.png';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 
+const useStyles = makeStyles((theme) => ({
+  imageStyle: {
+    height: '105%',
+    transform: 'rotate(5deg)',
+    width: '100px',
+    [theme.breakpoints.up('sm')]: {
+      width: '150px',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '200px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '150px',
+    },
+  },
+}));
 
 const HomePage = () => {
+  const classes = useStyles();
   const navigate = useNavigate();
 
-  const handleRegisterForm = ()=> {
-    navigate("/registrationForm")
-  }
+  const handleRegisterForm = () => {
+    navigate("/registrationForm");
+  };
 
   return (
     <div>
-       <Card sx={{background: 'linear-gradient(to right,#4772D9, #2899DB,#70CCE2)'}}>
-      <CardContent>
-        <Grid container  alignItems="center" >
-          {/* Left side */}
-          <Grid item xs={12} sm={7} padding="30px">
-            <Typography variant="h1" >
-              A Captivative Heading will be placed here.
-            </Typography>
-            <Typography variant="body1" gutterBottom sx={{mt:3,color:"white"}}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eos fuga laborum doloremque. Modi libero nam, fugit quasi quidem aliquam placeat officia possimus ad accusamus inventore eius, nesciunt iure deserunt.
-            </Typography>
-            <Button variant="contained"   onClick={()=> handleRegisterForm()} sx={{backgroundColor:"white",color:"#2899DB",mt:3}}>
-              Register Now
-            </Button>
-            <Button variant="outlined"  sx={{ ml: 1,color:"white",mt:3 }}>
-              <PlayCircleIcon sx={{mr:1}}/>
-              Watch Video
-            </Button>
-            <Box sx={{ backgroundColor: "white", color: "#20252C", padding: '5px 10px', mt: 1, display: 'flex', alignItems: 'center', borderRadius: '5px', width: 'fit-content' }}>
+      <Card sx={{ background: 'linear-gradient(to right, #4772D9, #2899DB, #70CCE2)', pb: 0, pt: 0 }}>
+        <CardContent sx={{ pb: '0 !important', pt: 0 }}>
+          <Grid container alignItems="center" columnSpacing={2}>
+            {/* Left side */}
+            <Grid item xs={12} sm={6} lg={6} md={6} sx={{ pt: "30px", pb: "30px", pr: "50px" }}>
+              <Typography variant="h1">
+                A Captivating Heading will be placed here.
+              </Typography>
+              <Typography variant="body1" gutterBottom sx={{ mt: 3, color: "white" }}>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium eos fuga laborum doloremque. Modi libero nam, fugit quasi quidem aliquam placeat officia possimus ad accusamus inventore eius, nesciunt iure deserunt.
+              </Typography>
+              <Button variant="contained" onClick={handleRegisterForm} sx={{ backgroundColor: "white", color: "#2899DB", mt: 3 }}>
+                Register Now
+              </Button>
+              <Button variant="outlined" sx={{ ml: 1, color: "white", mt: 3 }}>
+                <PlayCircleIcon sx={{ mr: 1 }} />
+                Watch Video
+              </Button>
+              <Box sx={{ backgroundColor: "white", color: "#20252C", padding: '5px 10px', mt: 1, display: 'flex', alignItems: 'center', borderRadius: '5px', width: 'fit-content' }}>
                 <Typography variant="body2" sx={{ fontWeight: 'bold', mr: 1 }}>
                   Powered By
                 </Typography>
@@ -50,80 +67,41 @@ const HomePage = () => {
                   sx={{ width: '70px', height: 'auto' }}
                 />
               </Box>
-            
+            </Grid>
+            {/* Right side */}
+            <Grid item xs={12} sm={6} lg={6} md={6}>
+              <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '12px', height: "400px" }}>
+                <Box
+                  component="img"
+                  src={Image1}
+                  alt="Image1"
+                  className={classes.imageStyle}
+                />
+                <Box
+                  component="img"
+                  src={Image2}
+                  alt="Image2"
+                  className={classes.imageStyle}
+                />
+                <Box
+                  component="img"
+                  src={Image3}
+                  alt="Image3"
+                  className={classes.imageStyle}
+                />
+                <Box
+                  component="img"
+                  src={Image4}
+                  alt="Image4"
+                  className={classes.imageStyle}
+                />
+              </Box>
+            </Grid>
           </Grid>
-          {/* Right side */}
-          <Grid item xs={12} sm={5}>
-           <Box
-           sx={{position: 'relative',width:"400px",height:"400px"}}
-           >
-            <Box
-            component = "img"
-            src={NetworksImage}
-            alt= "Image1"
-            sx={{position:'absolute',
-              top:'10px',left:'10px',width:"400px",
-              height:"400px",
-            }}
-
-            />
-              <Box
-            component = "img"
-            src={Image1}
-            alt= "Image1"
-            sx={{position:'absolute',
-              top:'175px',left:'175px',width:"70px",
-              height:"70px",
-            }}
-
-            />
-              <Box
-            component = "img"
-            src={Image2}
-            alt= "Image1"
-            sx={{position:'absolute',
-              top:'30px',left:'40px',width:"70px",
-              height:"70px",
-            }}
-
-            />
-              <Box
-            component = "img"
-            src={Image3}
-            alt= "Image1"
-            sx={{position:'absolute',
-              top:'10px',left:'300px',width:"70px",
-              height:"70px",
-            }}
-
-            />
-            <Box
-            component = "img"
-            src={Image4}
-            alt= "Image1"
-            sx={{position:'absolute',
-              top:'300px',left:'300px',width:"70px",
-              height:"70px",
-            }}
-            />
-            <Box
-            component = "img"
-            src={Image5}
-            alt= "Image5"
-            sx={{position:'absolute',
-              top:'250px',left:'10px',width:"230px",
-              height:"230px",
-            }}
-
-            />
-
-           </Box>
-          </Grid>
-        </Grid>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
