511da053bf50e287d41abd6ebced33b24a85d323
Anshika Jain <ansuu100@gmail.com>
2024-07-02 08:58:03 +0530

    cross origin fixed

--- src/main/java/in/kpmg/auro/project/controllers/DashboardController.java
+++ src/main/java/in/kpmg/auro/project/controllers/DashboardController.java
@@ -3,11 +3,12 @@ package in.kpmg.auro.project.controllers;
 import in.kpmg.auro.project.dtos.ApiResponse2;
 import in.kpmg.auro.project.services.DashboardServices;
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class DashboardController {
 
     @Autowired
